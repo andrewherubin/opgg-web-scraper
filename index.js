a0119de1850962a1474cca896ff1cc44c9f412c1
Andrew Herubin <andrewherubin@outlook.com>
2022-12-14 11:39:05 -0600

    remove frontend

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const port = process.env.PORT || 3333
 
 const app = Express()
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: __dirname+'/public/'})
-})
-
 app.listen(port, () => {
     console.log(`Server listening on port ${port}...`)
 })
